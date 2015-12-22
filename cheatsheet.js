0d95898d76639f37b8bab8a1d5864c96faa04f86
Scott Yim <scottyim@gmail.com>
2015-12-21 20:01:09 -0500

    structure

--- cheatsheet.js
+++ cheatsheet.js
@@ -1,1 +1,50 @@
 // JavaScript Cheat Sheet
+
+/* CONTENTS
+1. Variables
+2. Debugging (alerts, comments, the console)
+3. Different data types
+4. Arrays
+5. Testing
+6. Logic */
+
+// 1. Variables
+// - used to store values
+// - can include any data type
+
+var myName = "Scott"; // (string)
+var myAge = 25; // (number)
+
+// 2. Debugging
+// Can use 1 of three methods:
+
+      // 1. alerts
+
+      // 2. comments
+
+      // 3. console
+      
+// 3. Different data types
+// String - "Hello World"
+// Number - 1, 2, 100 (all numbers in JS are ﬂoats)
+// Boolean - true or false
+// Undefined (no value)
+// Arrays - hold a collection of data, of any data type (more below)
+// Objects - like an array, with defined keys (more below)
+
+// 4. Arrays
+
+var fruits = ["apple", "banana", "blueberry"]
+// Index of apple = 0, banana = 1, blueberry = 2
+
+var vegetables = ["broccoli", "carrot", "celery"]
+
+// Can be combined inside of one, multi-dimensional array, as follows:
+var healthy_foods = [fruits, vegetables]
+
+   // 4.5 Objects
+   var fruits = {color: 'blue', taste: 'sweet'}
+
+// 5. Testing
+
+// 6. Logic
